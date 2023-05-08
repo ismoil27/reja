114d16723f68b04f3431707b352078bfa6f5295e
ismoil27 <abduraimovismoil98@gmail.com>
2023-05-08 13:17:22 +0900

    feat: develop auhtor router page

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 const http = require("http");
-// 1
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// 1
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +29,10 @@ app.set("view engine", "ejs");
 
 app.post("/create-item", function (req, res) {
   //  TODO:
+});
+
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
