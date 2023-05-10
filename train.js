a9f5a26f6e179af021add5d71663f0454162d4c3
ismoil27 <abduraimovismoil98@gmail.com>
2023-05-10 15:37:24 +0900

    fix: modify train js file

--- train.js
+++ train.js
@@ -17,7 +17,7 @@ function maslahatBering(a, callback) {
   else if (a > 40 && a <= 50) callback(null, list[3]);
   else if (a > 50 && a <= 60) callback(null, list[4]);
   else {
-    setTimeout(function () {
+    setInterval(function () {
       callback(null, list[5]);
     }, 5000);
   }
@@ -33,3 +33,59 @@ maslahatBering(65, (err, data) => {
 });
 
 console.log("Passed here 1");
+
+// async function maslahatBering(a, callback) {
+//   if (typeof a !== "number") throw new Error("Iltimos raqam yozing");
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30) return list[1];
+//   else if (a > 30 && a <= 40) return list[2];
+//   else if (a > 40 && a <= 50) return list[3];
+//   else if (a > 50 && a <= 60) return list[4];
+//   else {
+//     return new Promise((resolve, reject) => {
+//       //   setTimeout(() => {
+//       //     resolve(list[5]);
+//       //   }, 5000);
+
+//       setInterval(() => {
+//         resolve(list[1]);
+//       }, 1000);
+//     });
+
+//     // return list[5];
+//     // setTimeout(function () {
+//     // }, 5000);
+//   }
+// }
+
+// console.log("Passed here 0");
+
+// maslahatBering("hello")
+//   .then((data) => {
+//     console.log(`Javob: ${data}`);
+//   })
+//   .catch((err) => {
+//     console.log(`${err}`);
+//   });
+
+// console.log("Passed here 1");
+
+// async/await
+// async function run() {
+//   let javob = await maslahatBering(65);
+//   console.log(javob);
+
+//   //   javob = await maslahatBering(31);
+//   //   console.log(javob);
+
+//   //   javob = await maslahatBering(41);
+//   //   console.log(javob);
+
+//   //   javob = await maslahatBering(51);
+//   //   console.log(javob);
+
+//   //   javob = await maslahatBering(61);
+//   //   console.log(javob);
+// }
+
+// run();
