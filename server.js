a86cf1aa5b7786ec128a509b69c7896af32bf533
ismoil27 <abduraimovismoil98@gmail.com>
2023-05-12 22:03:22 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -36,11 +36,11 @@ app.get("/author", function (req, res) {
 });
 
 app.get("/", function (req, res) {
-  res.render("harid");
+  res.render("reja");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-  console.log(`server is running on port ${PORT}`);
+  console.log(`server is running on port ${PORT}, http://localhost:${PORT}`);
 });
